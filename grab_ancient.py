120f0986dfe2e3756336da3006afce74c6369693
comex <comexk@gmail.com>
2017-07-07 15:59:26 -0400

    extract stuff

--- grab_ancient.py
+++ grab_ancient.py
@@ -1,35 +1,58 @@
-import re, os
+import re, os, sys
 import util
 def normalize(rule_text):
-    return re.sub('[^a-z0-9]', '', rule_text.lower())
+    if len(rule_text)> 1000000:
+        print rule_text
+        die
+    return re.sub('[^a-z0-9]*', '', rule_text.lower())
 zefram = open('old_zefram_rules_text.txt').read()
 zefram_texts = {}
 for text in re.findall(re.compile('\n\n(?:\[orphaned )?text:\n\n(.*?)\n\n(?! )', re.S), zefram):
     zefram_texts[normalize(text)] = text
 
-
+my_texts = {}
+seen_exactly = set()
 def handle_text(filename, text):
     for full, header, rest in re.findall('^((Rule [0-9]+[^\n]*\([^\n\)]*)\)[ \t]*\n(.*?)\n\n(?![ 0-9]))', text, re.M | re.S):
+        if full in seen_exactly:
+            continue
+        seen_exactly.add(full)
         title = None
-        if '\n' not in rest:
-            print repr(full)
-            print repr(rest)
-            print path
-            die
         if not rest.startswith('\n') and '\n' in rest:
             title, rest = rest.split('\n', 1)
-        print rest
-        print '###'
-for root, dirs, files in os.walk('agora-archives'):
+        extra = None
+        m = re.match('\n([0-9]+\. [^\n]*)\n\n(.*)$', rest, re.S)
+        if m:
+            extra, rest = m.groups()
+        normalized = normalize(rest)
+        data = {
+            'header': header,
+            'title': title,
+            'extra': extra,
+            'rest': rest,
+        }
+        my_texts.setdefault(normalized, []).append(data)
+#handle_text('a', open('/Users/comex/agora_vanyel0/agora/logs/official/1998.10.20').read())
+#sys.exit(0)
+for root, dirs, files in os.walk('zeframs-archives'):
     for filename in files:
         path = os.path.join(root, filename)
+        #print path
         if not os.path.isfile(path): continue
         if 'theses/' in path or 'cfj/' in path or filename == 'discussion':
             continue # some fake rules here
         if filename.endswith(',v'):
-            for rev, text in util.revs_of_rcs_file(filename):
-                handle_text(filename + ':' + rev, text)
+            print 'doing rcs file %s...' % path
+            for rev, text in util.revs_of_rcs_file(path):
+                handle_text(path + ':' + rev, text)
+            print '...done.'
         else:
+            print path
             with open(path, 'r') as fp:
                 text = fp.read()
-                handle_text(filename, text)
+                handle_text(path, text)
+for norm in zefram_texts:
+    if norm not in my_texts:
+        print 'missing:'
+        print zefram_texts[norm]
+        print '--'
--- util.py
+++ util.py
@@ -1,6 +1,6 @@
 import subprocess, re, os
 def x_check_output(args, **kwargs):
-    p = subprocess.Popen(args, stdout=subprocess.PIPE, **kwargs)
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, **kwargs)
     stdout, stderr = p.communicate()
     return stdout
 def revs_of_rcs_file(filename):
@@ -9,5 +9,5 @@ def revs_of_rcs_file(filename):
     filename = os.path.abspath(filename)
     log = x_check_output(['rcs', 'rlog', filename], env=env)
     for rev in re.findall('-----\nrevision (\S+)', log):
-        text, _ = x_check_output(['rcs', 'co', '-p'+rev, filename], env=env)
+        text = x_check_output(['rcs', 'co', '-p'+rev, filename], env=env)
         yield rev, text
