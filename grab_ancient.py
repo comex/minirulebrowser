f16fdb9e1b45d75cd2fe2b3e84ebd14fb637b759
comex <comexk@gmail.com>
2017-07-08 13:13:22 -0400

    ...

--- grab_ancient.py
+++ grab_ancient.py
@@ -1,40 +1,139 @@
-import re, os, sys
+import os, sys
 import util
-def normalize(rule_text):
-    if len(rule_text)> 1000000:
-        print rule_text
-        die
-    return re.sub('[^a-z0-9]*', '', rule_text.lower())
-zefram = open('old_zefram_rules_text.txt').read()
-zefram_texts = {}
-for text in re.findall(re.compile('\n\n(?:\[orphaned )?text:\n\n(.*?)\n\n(?! )', re.S), zefram):
-    zefram_texts[normalize(text)] = text
+import regex # not re
 
 my_texts = {}
 seen_exactly = set()
-def handle_text(filename, text):
-    for full, header, rest in re.findall('^((Rule [0-9]+[^\n]*\([^\n\)]*)\)[ \t]*\n(.*?)\n\n(?![ 0-9]))', text, re.M | re.S):
+fsfr_regex = regex.compile(r'''
+    ^ # start of line
+    (?P<full>
+        (?P<header>
+            Rule\ (?P<number>[0-9]+)
+            (?: # revision number?
+                /(?P<revnum>[0-9]+)
+            )?
+            [^\n]*
+            # mutability/power marking
+            \([^\n\)]*\)
+        )
+        (?P<newline>[ \t]*\n)
+        (?:
+            # title line
+            (?P<title>\S[^\n]*?)
+            (?&newline)
+        )?
+        (?&newline)
+        (?:
+            # old fashioned rule number?
+            (?P<extra>
+                [0-9]+\.\ .*?
+            )
+            (?&newline)
+            (?&newline)
+        )?
+        # main rule text
+        (?P<text>.*?)
+        (?&newline)
+        (?&newline)
+        # there may be some []-delimited annotations
+        (?P<annotations>
+            (?:
+                \[
+                # allow nested []s, lol recursive regex
+                (?P<nested>
+                    [^\[\]]* |
+                    \[(?&nested)\]
+                )*
+                # ending ] should be followed by \n\n, perhaps with excess whitespace
+                \]
+                (?&newline)
+                (?&newline)
+            )*
+        )
+        # there may be history
+        (?P<history>
+            History:
+            (?&newline)
+            (?P<thehist>
+                (?&newline)
+                .*?
+                (?&newline)
+                (?&newline)
+            )
+        )?
+        # end on something that is not indented, nor an old-fashioned numbered rule
+        (?![ 0-9])
+    )
+''', regex.M | regex.S)
+def find_stdformat_rules(text, seen_exactly_set):
+    for m in fsfr_regex.finditer(text):
+        g = m.groupdict()
         if full in seen_exactly:
             continue
         seen_exactly.add(full)
-        title = None
-        if not rest.startswith('\n') and '\n' in rest:
-            title, rest = rest.split('\n', 1)
-        extra = None
-        m = re.match('\n([0-9]+\. [^\n]*)\n\n(.*)$', rest, re.S)
-        if m:
-            extra, rest = m.groups()
-        normalized = normalize(rest)
+        history = None
+        if g['history'] is not None:
+            history = list(split_history(g['thehist']))
         data = {
+            'number': int(g['number']),
+            'revnum': int(g['revnum') if g['revnum'] else None,
+            'title': g['title'] or None,
+            'extra': g['extra'] or None,
+            'text': text,
+            'annotations': g['annotations'],
+            'history': history,
+        }
+
+
+def split_history(history):
+    for line in regex.split('\n(?!  )', history):
+        yield regex.replace('\s+', ' ', line)
+def handle_filetext(path, filetext):
+    for full, header, number, text in re.findall('^((Rule ([0-9]+)[^\n]*\([^\n\)]*)\)[ \t]*\n(.*?)\n\n(?![ 0-9]))', filetext, re.M | re.S):
+        if full in seen_exactly:
+            continue
+        seen_exactly.add(full)
+        number = int(number)
+        title = None
+        if not text.startswith('\n') and '\n' in text:
+            title, text = text.split('\n', 1)
+        extra = None
+        m = re.match('\n([0-9]+\. [^\n]*)\n\n(.*)$', text, re.S)
+        if m:
+            extra, text = m.groups()
+        normalized = normalize(text)
+        data = {
+            'number': number,
             'header': header,
             'title': title,
             'extra': extra,
-            'rest': rest,
+            'text': text,
         }
         my_texts.setdefault(normalized, []).append(data)
-#handle_text('a', open('/Users/comex/agora_vanyel0/agora/logs/official/1998.10.20').read())
-#sys.exit(0)
-for root, dirs, files in os.walk('zeframs-archives'):
+    # alternate old format
+    if 'usenet0/rga' in path:
+        for header, number, letter, text in re.findall('^(([0-9]{3})([a-z]?)\.\s+)(.*?)\n\s*\n', filetext, re.M | re.S):
+            title = None
+            number = int(number)
+            if number >= 319 and number not in (330, 436):
+                # have title
+                title, text = text.split('\n', 1)
+                header += title
+                title = title.rstrip(':')
+            else:
+                text = ' ' * len(header) + text
+                header = header.rstrip()
+            normalized = normalize(text)
+            data = {
+                'number': number,
+                'header': header,
+                'title': title,
+                'extra': letter or None,
+                'text': text,
+            }
+            my_texts.setdefault(normalized, []).append(data)
+rootpath = sys.argv[1] if len(sys.argv) > 1 else 'zeframs-archives'
+for root, dirs, files in os.walk(rootpath):
     for filename in files:
         path = os.path.join(root, filename)
         #print path
@@ -44,13 +143,19 @@ for root, dirs, files in os.walk('zeframs-archives'):
         if filename.endswith(',v'):
             print 'doing rcs file %s...' % path
             for rev, text in util.revs_of_rcs_file(path):
-                handle_text(path + ':' + rev, text)
+                handle_filetext(path + ':' + rev, text)
             print '...done.'
         else:
-            print path
+            #print path
             with open(path, 'r') as fp:
                 text = fp.read()
-                handle_text(path, text)
+                handle_filetext(path, text)
+with open('temp.txt', 'w') as fp:
+    for datas in my_texts.itervalues():
+        for data in datas:
+            fp.write('<<%s>>\n\n' % data['text'])
+        
+#sys.exit(0)
 for norm in zefram_texts:
     if norm not in my_texts:
         print 'missing:'
--- util.py
+++ util.py
@@ -1,13 +1,38 @@
-import subprocess, re, os
+import subprocess, regex, os
+from multiprocessing import Pool
 def x_check_output(args, **kwargs):
     p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, **kwargs)
     stdout, stderr = p.communicate()
     return stdout
-def revs_of_rcs_file(filename):
-    env = dict(os.environ.iteritems())
-    env['RCS_MEM_LIMIT'] = '99999' # kb
-    filename = os.path.abspath(filename)
-    log = x_check_output(['rcs', 'rlog', filename], env=env)
-    for rev in re.findall('-----\nrevision (\S+)', log):
-        text = x_check_output(['rcs', 'co', '-p'+rev, filename], env=env)
-        yield rev, text
+def revs_of_rcs_file(path):
+    path = os.path.abspath(path)
+    log = x_check_output(['rcs', 'rlog', path], env=rcsenv)
+    split = regex.split('\n(?:----------------------------|=============================================================================)\n', log)
+    assert split[-1].strip() == ''
+    split = split[1:-1]
+    todo = []
+    for revinfo in split:
+        m = regex.match('revision ([^\n]*)\ndate: ([^;]*)[^\n]*\n(.*)', revinfo, regex.S)
+        if not m:
+            raise Exception("can't parse output from rcs: %r" % revinfo)
+        rev, date, revlog = m.groups()
+        todo.append({'rev': rev, 'date': date, 'revlog': revlog, 'path': path})
+    return Pool().map(revs_of_rcs_file_do_one, todo)
+
+rcsenv = dict(os.environ.iteritems())
+rcsenv['RCS_MEM_LIMIT'] = '99999' # kb
+
+def revs_of_rcs_file_do_one(info):
+    text = x_check_output(['rcs', 'co', '-p'+info['rev'], info['path']], env=rcsenv)
+    info['text'] = text
+    del info['path']
+    print '#', info['rev']
+    return info
+
+def normalize(rule_text):
+    if len(rule_text)> 1000000:
+        print rule_text
+        die
+    return re.sub('[^a-z0-9]*', '', rule_text.lower())
+
+for info in revs_of_rcs_file('/Users/comex/agora/current_flr.txt,v'): print info
